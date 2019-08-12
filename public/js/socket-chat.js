f71fbb4d33e249e6d958dea2f7c856adbac2de50
Cristian <cristian930714@hotmail.com>
2019-08-11 23:54:57 -0500

    chat finalizado con jquery

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,19 +1,27 @@
 var socket = io();
+
 var params = new URLSearchParams(window.location.search);
+
 if (!params.has('nombre') || !params.has('sala')) {
-    window.location = 'index.html'
+    window.location = 'index.html';
     throw new Error('El nombre y sala son necesarios');
 }
+
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados: ', resp);
+        // console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -26,7 +34,7 @@ socket.on('disconnect', function() {
 
 // Enviar información
 // socket.emit('crearMensaje', {
-//     usuario: 'Fernando',
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
@@ -34,18 +42,21 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    // console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
-// escuchar cuando un usuario entra o sale del chat
+
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log(personas);
-
+    // console.log(personas);
+    renderizarUsuarios(personas);
 });
 
-//Mensajes privados
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('mensaje privado: ', mensaje)
-})
+
+    console.log('Mensaje Privado:', mensaje);
+
+});
